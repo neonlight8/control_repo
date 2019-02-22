8b5b2ded8177434d103b97eb826514d671fe565a
neonlight8 <47842787+neonlight8@users.noreply.github.com>
2019-02-22 15:21:46 +1100

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -3,4 +3,10 @@ node default {
 node 'ec2-52-64-37-217.ap-southeast-2.compute.amazonaws.com' {
  include role::master_server
 }
+node /^web/ {
+ include role::app_server
+}
+node /^db/ {
+ include role::db_server
+}
 
