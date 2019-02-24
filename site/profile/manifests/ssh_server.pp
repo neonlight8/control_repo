3837e5d7b3b578b265873f9459f2329367d6e3bd
neonlight8 <47842787+neonlight8@users.noreply.github.com>
2019-02-24 23:39:12 +1100

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
 		ensure => present,
 		user   => 'root',
 		type   => 'ssh-rsa',
-		key    => '',
+		key    => ' AAAAB3NzaC1yc2EAAAADAQABAAABAQDMua8EPW/SqeuefZlyqPWnVeICrAesaibVagyKn3m1d+dIgWI52k902ToE6vAadSGnfYChd9shvuTIDKz3nSiS3rfos74diF+9L/eiVNd+UiT48rrL5D2rP0+168yHKHq6NJrAdUSnK1j1SjSsSy3bJKzP1g0Rg/MOml6ruGyILv/EPOJDy1DE8gdBHhRUqiTSIA8wrNgyQHzJgoiycfKtswquAzEhXtNG9EjWaTdJX/8ghtG3gLoi2Bj3gEgcMWYSS9JEU+if/nHVTDCjW2jzZTgMbbkRYy24EkxOCGiwTNENHa3qZnLDZh0R499OtyHIM5r/zjvx0NgWCK+o8xYT',
 	}  
 }
